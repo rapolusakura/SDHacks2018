51d98fd671d25b3a8f75dcfb7b61046dda32c931
Sakura Rapolu <rapolusakura@gmail.com>
2018-10-13 11:26:40 -0700

    installed pods

---
+++ SDHacks2018/SDHacks2018/View Controllers/CreateContractViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CreateContractViewController.swift
+//  SDHacks2018
+//
+//  Created by Sakura Rapolu on 10/13/18.
+//  Copyright © 2018 SDHacks. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CreateContractViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet weak var createContractButton: UIButton!
+    
+    @IBAction func createContractButtonPressed(_ sender: Any) {
+        print("create contract button pressed") 
+    }
+}
+
+
